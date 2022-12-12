1559460954626e850c0dbc084270f18704c8a999
Kbgjtn <kambingjan31@gmail.com>
2022-12-12 14:15:12 +0700

    adding header response to client

--- src/application/configurations/setup.ts
+++ src/application/configurations/setup.ts
@@ -2,9 +2,11 @@ import { Application } from "express";
 import { cors } from "../middlewares";
 import bodyParser from "body-parser";
 import router from "../../presentation/routers";
+import { header } from "./header";
 
 const setup = (app: Application) => {
 	app.use(cors);
+	app.use(header);
 	app.use(
 		bodyParser.json({ limit: "100kb" }),
 		bodyParser.urlencoded({
